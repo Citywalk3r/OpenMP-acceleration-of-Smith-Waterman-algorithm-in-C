f0ecae4ce29ed1361ec4d257e1668f2e7c832150
StrayLamb2 <mpampinis@gmail.com>
2019-03-11 03:59:03 +0200

    "mia mikrh allagh"

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+set username StrayLamb2
+set pass Secretcode2!
 read -p "Commit comment: " s
 git add .
 git commit -m "\"${s}\""
 git push origin Stray
+
+expect "*"
+send "${username}\r"
+
+expect "*"
+send "${pass}\r"
+
+expect eof
